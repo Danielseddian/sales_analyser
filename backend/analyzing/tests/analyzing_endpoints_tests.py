15af872afbf18e5376e0801ce88078626359d57b
Денис <danielseddian@yandex.ru>
2024-11-18 09:26:15 +0300

    pipelines

--- backend/analyzing/tests/analyzing_endpoints_tests.py
+++ backend/analyzing/tests/analyzing_endpoints_tests.py
@@ -1,11 +1,9 @@
 import pytest
-from django.test import override_settings
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from analyzing.models import SalesData
 from uuid import uuid4
-from django.conf import settings
 
 
 @pytest.mark.django_db
@@ -35,81 +33,64 @@ class TestSalesDataViews:
         )
 
     def test_sales_data_incorrect_file_upload(self, client):
-        with override_settings(DATABASE_ENGINE=0, CACHE_ENGINE=0):
-            url = reverse('api:analyzing:upload-sales-data')
+        url = reverse('api:analyzing:upload-sales-data')
 
-            with open('dummy_file.xml', 'wb') as f:
-                f.write(b"""
-                    <sales_data date="2024-01-01">
-                        <products>
-                            <product>test</product>
-                        </products>
-                    </sales_data>
-                    """)
+        with open('dummy_file.xml', 'wb') as f:
+            f.write(b"""
+                <sales_data date="2024-01-01">
+                    <products>
+                        <product>test</product>
+                    </products>
+                </sales_data>
+                """)
 
-            with open('dummy_file.xml', 'rb') as f:
-                response = client.post(url, {'xml_file': f}, format='multipart')
+        with open('dummy_file.xml', 'rb') as f:
+            response = client.post(url, {'xml_file': f}, format='multipart')
 
-            assert response.status_code == status.HTTP_400_BAD_REQUEST
-            assert 'xml_file' in response.data
-            assert response.data['xml_file'] == 'Неправильная структура продукта XML.'
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'xml_file' in response.data
+        assert response.data['xml_file'] == 'Неправильная структура продукта XML.'
 
     def test_get_sales_data_result(self, client, sales_data):
-        with override_settings(
-            DATABASES={'default': settings.DATABASE_ENGINE[0]},
-            CACHES={'default': settings.CACHE_ENGINES[0]},
-        ):
-            sales_data.reported_at = '2024-01-01T00:00:00Z'
-            sales_data.report = '{"report": "test report"}'
-            sales_data.save()
-            url = reverse('api:analyzing:get-sales-data-result', kwargs={'uuid': sales_data.uuid})
+        sales_data.reported_at = '2024-01-01T00:00:00Z'
+        sales_data.report = '{"report": "test report"}'
+        sales_data.save()
+        url = reverse('api:analyzing:get-sales-data-result', kwargs={'uuid': sales_data.uuid})
 
-            response = client.get(url)
+        response = client.get(url)
 
-            assert response.status_code == status.HTTP_200_OK
-            assert 'report' in response.data
-            assert 'test report' in response.data['report']
+        assert response.status_code == status.HTTP_200_OK
+        assert 'report' in response.data
+        assert 'test report' in response.data['report']
 
     def test_get_sales_data_result_not_found(self, client):
-        with override_settings(
-            DATABASES={'default': settings.DATABASE_ENGINE[0]},
-            CACHES={'default': settings.CACHE_ENGINES[0]},
-        ):
-            url = reverse('api:analyzing:get-sales-data-result', kwargs={'uuid': uuid4()})
+        url = reverse('api:analyzing:get-sales-data-result', kwargs={'uuid': uuid4()})
 
-            response = client.get(url)
+        response = client.get(url)
 
-            assert response.status_code == status.HTTP_404_NOT_FOUND
-            assert 'detail' in response.data
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert 'detail' in response.data
 
     def test_get_sales_data_result_in_progress(self, client, sales_data):
-        with override_settings(
-            DATABASES={'default': settings.DATABASE_ENGINE[0]},
-            CACHES={'default': settings.CACHE_ENGINES[0]},
-        ):
-            sales_data.reported_at = None
-            sales_data.save()
+        sales_data.reported_at = None
+        sales_data.save()
 
-            url = reverse('api:analyzing:get-sales-data-result', kwargs={'uuid': sales_data.uuid})
+        url = reverse('api:analyzing:get-sales-data-result', kwargs={'uuid': sales_data.uuid})
 
-            response = client.get(url)
+        response = client.get(url)
 
-            assert response.status_code == status.HTTP_202_ACCEPTED
-            assert 'message' in response.data
-            assert 'Отчёт ещё формируется' in response.data['message']
+        assert response.status_code == status.HTTP_202_ACCEPTED
+        assert 'message' in response.data
+        assert 'Отчёт ещё формируется' in response.data['message']
 
     def test_get_sales_data_result_with_error(self, client, sales_data):
-        with override_settings(
-            DATABASES={'default': settings.DATABASE_ENGINE[0]},
-            CACHES={'default': settings.CACHE_ENGINES[0]},
-        ):
-            sales_data.errors_log = 'Error processing the file'
-            sales_data.save()
+        sales_data.errors_log = 'Error processing the file'
+        sales_data.save()
 
-            url = reverse('api:analyzing:get-sales-data-result', kwargs={'uuid': sales_data.uuid})
+        url = reverse('api:analyzing:get-sales-data-result', kwargs={'uuid': sales_data.uuid})
 
-            response = client.get(url)
+        response = client.get(url)
 
-            assert response.status_code == status.HTTP_204_NO_CONTENT
-            assert 'detail' in response.data
-            assert 'Error processing the file' in response.data['detail']
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert 'detail' in response.data
+        assert 'Error processing the file' in response.data['detail']
